0e1a24b9827682bd6ee82fefd7681473ba7894ea
CaraDelevingne <15902804674@163.com>
2017-09-20 22:35:58 +0800

    add bitwise

---
+++ JavaScript-TheGoodParts/bitwise/bitwise.js
@@ -0,0 +1,18 @@
+
+// ~ not运算符
+let num = 10;
+let num2 = ~num;
+
+console.log(num2); // -11
+
+// << 左位移
+let leftNum = 2;
+let leftNum2 = leftNum << 2;
+
+console.log(leftNum2); // 8
+
+// 无符号右位移
+let rightNum = 12;
+let rightNum2 = rightNum >> 2;
+
+console.log(rightNum2) // 3
